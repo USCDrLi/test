bb19c966e54f37d82a1741783e4f676be4a09312
USCDrLi <lee9826@hotmail.com>
2016-03-14 09:55:28 -0700

    upload one file

---
+++ DSP28_Gpio.c
@@ -0,0 +1,29 @@
+
+
+
+#include "DSP28_Device.h"
+
+
+
+void InitGpio(void)
+{
+
+     EALLOW;
+     
+    
+     GpioMuxRegs.GPAMUX.bit.PWM1_GPIOA0=1;     
+     GpioMuxRegs.GPAMUX.bit.PWM2_GPIOA1=1;     
+
+     GpioMuxRegs.GPAMUX.bit.PWM3_GPIOA2=1;     
+     GpioMuxRegs.GPAMUX.bit.PWM4_GPIOA3=1;     
+
+     GpioMuxRegs.GPAMUX.bit.PWM5_GPIOA4=1;     
+     GpioMuxRegs.GPAMUX.bit.PWM6_GPIOA5=1;     
+
+     EDIS;
+
+}	
+	
+//===========================================================================
+// No more.
+//===========================================================================
